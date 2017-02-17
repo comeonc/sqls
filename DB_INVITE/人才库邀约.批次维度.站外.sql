73319bb75d73195be668639efc6e342b3935c640
comeonc <comeonc@126.com>
2017-02-17 10:00:24 +0800

    人才库

--- DB_INVITE/人才库邀约.批次维度.站外.sql
+++ DB_INVITE/人才库邀约.批次维度.站外.sql
@@ -2,12 +2,12 @@ SELECT
 	date(max(add_date)) days,
 	corp_id,
 	info_id '批次',
-	count(1) '邀请',
-	sum(has_read) '已读',
-	sum(CASE operation WHEN 1 THEN 1 ELSE 0 END ) '接受',
-	format(100*sum(has_read)/count(1),2) '阅读率%',
-	format(100*sum(CASE operation WHEN 1 THEN 1 ELSE 0 END )/sum(has_read),2) '接受率%',
-	format(100*sum(CASE operation WHEN 1 THEN 1 ELSE 0 END )/count(1),2) '总接受率%'
+	count(DISTINCT uid) '邀请',
+	count(DISTINCT CASE WHEN has_read = 1 THEN uid END ) '已读',
+	count(DISTINCT CASE WHEN operation = 1 THEN uid END ) '接受',
+	format(100*count(DISTINCT CASE WHEN has_read = 1 THEN uid END )/count(DISTINCT uid),2) '阅读率%',
+	format(100*count(DISTINCT CASE WHEN operation = 1 THEN uid END )/count(DISTINCT CASE WHEN has_read = 1 THEN uid END ),2) '接受率%',
+	format(100*count(DISTINCT CASE WHEN operation = 1 THEN uid END )/count(DISTINCT uid),2) '总接受率%'
 FROM
 	tb_c2c_corp_invitation_101
 WHERE 
